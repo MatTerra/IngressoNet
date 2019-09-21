b96e29b78bbad95bd0619860a3005c70ea84d2ee
MatTerra <mateus.b.s.terra@gmail.com>
2019-09-21 16:00:51 -0300

    Improved isValidCPF

--- src/usuario.cpp
+++ src/usuario.cpp
@@ -9,31 +9,38 @@ bool Usuario::isValidCPF(std::string cpf){
   if(cpf.length()!=14){
       return false;
   }
-  cpf.erase(std::remove(cpf.begin(), cpf.end(), '.'), cpf.end());
-  cpf.erase(std::remove(cpf.begin(), cpf.end(), '-'), cpf.end());
-  const char* cpfNumbers = cpf.c_str();
+
+  cpf = Usuario::getCPFNumbers(cpf);
+
   int soma = 0;
   for(int cont = 0; cont < 9; cont++){
-    soma += (cpfNumbers[cont]-'0')*(10-cont);
+    soma += (cpf.at(cont)-'0')*(10-cont);
   }
   int resultado = (11-(soma%11));
   if(resultado == 10 || resultado == 11){
     resultado = 0;
   }
-  if(resultado != (cpfNumbers[9]-'0')){
+  if(resultado != (cpf.at(9)-'0')){
       return false;
   }
+
   soma = 0;
   for(int cont = 0; cont < 10; cont++){
-    soma += (cpfNumbers[cont]-'0')*(11-cont);
+    soma += (cpf.at(cont)-'0')*(11-cont);
   }
   resultado = (11-(soma%11));
   if(resultado == 10 || resultado == 11){
     resultado = 0;
   }
-
-  if(resultado != (cpfNumbers[10]-'0')){
+  if(resultado != (cpf.at(10)-'0')){
       return false;
   }
+
   return true;
+}
+
+std::string Usuario::getCPFNumbers(std::string cpf){
+  cpf.erase(std::remove(cpf.begin(), cpf.end(), '.'), cpf.end());
+  cpf.erase(std::remove(cpf.begin(), cpf.end(), '-'), cpf.end());
+  return cpf;
 }
