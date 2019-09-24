c9bbec952b27aefefb7e01d2070052e7990335fc
bruno-bmc <castrobruno84@gmail.com>
2019-09-24 20:26:05 -0300

    Validações para entradas do jogo

--- src/jogo.cpp
+++ src/jogo.cpp
@@ -1,5 +1,65 @@
 #include "jogo.h"
+#include <string>
 
-Jogo::Jogo()
+Jogo::Jogo(int codigo, int tipo, std::string estado, std::string cidade, std::string nome_do_jogo)
 {
+    //ctor
+    cin >> codigo;
+    cin.cls();              //Substituir cls por clear antes da entrega
+
+    this-> codigo = codigo;
+
+    cin >> estado;
+    cin.cls();
+
+    this-> estado = estado;
+
+    getline(cin, cidade);
+    cin.cls();
+
+    this-> cidade = cidade;
+
+    cin >> tipo;
+    cin.cls();
+
+    this-> tipo = tipo;
+
+    getline(cin, nome_do_jogo);
+    cin.cls();
+
+    this-> nome_do_jogo = nome_do_jogo;
+
+}
+
+Jogo::~Jogo(){
+    //dtor
+
+}
+
+bool IsValidCodigo(){
+    bool valido = true;
+    if(codigo.length != 3){
+        valido = false;
+    }
+    return valido;
+}
+
+bool IsValidEstado(){
+
+}
+
+bool IsValidCidade(){
+
+}
+
+bool IsValidTipo(){
+    bool valido = true;
+    if(tipo < 1 || tipo > 4){
+        valido = false;
+    }
+    return valido;
+}
+
+bool IsValidNome(){
+
 }
--- src/jogo.h
+++ src/jogo.h
@@ -4,7 +4,28 @@
 class Jogo
 {
 public:
+    /**default constructor*/
     Jogo();
+    /**default destructor*/
+    virtual~Jogo();
+    /**validations**/
+    bool IsValidCodigo();
+
+    bool IsValidEstado();
+
+    bool IsValidCidade();
+
+    bool IsValidTipo();
+
+    bool IsValidNome();
+
+private:
+    int codigo;
+    int tipo;
+    std::string estado;
+    std::string cidade;
+    std::string nome_do_jogo;
+
 };
 
 #endif // JOGO_H
