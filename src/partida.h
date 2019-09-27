6ce60b86871554ebc83733291b986e6dc46511c2
ThiagoPais <34371661+ThiagoPais@users.noreply.github.com>
2019-09-27 15:01:08 -0300

    Update partida.h

--- src/partida.h
+++ src/partida.h
@@ -8,10 +8,42 @@
 class Partida{
 
 public:
+	/** Construtor padrão */
 	Partida(Cidade& cidade, float preco, int disponibilidade, Usuario& responsavel, Data data, Jogo jogo):
 	cidade(cidade), preco(preco), disponibilidade(disponibilidade), responsavel(responsavel), data(data), jogo(jogo){};
 
+	/** Vende um ingresso da partida a um usuário */
 	bool venderIngresso(Usuario& responsavel);
+
+	/** Método de acesso a codigo.
+     	* \return O valor atual de codigo
+    	*/
+   	int getCodigo() const { return codigo; }
+
+	/** Método de alteração de codigo.
+ 	* \param codigo Novo valor de codigo
+ 	*/
+	void setCodigo(int codigo) { this->codigo = codigo; }
+
+	/** Método de acesso a preco.
+     	* \return O valor atual de preco
+    	*/
+   	float getPreco() const { return preco; }
+
+	/** Método de alteração de preco.
+ 	* \param preco Novo valor de preco
+ 	*/
+	void setPreco(float preco) { this->preco = preco; }
+
+	/** Método de acesso a disponibilidade.
+     	* \return O valor atual de disponibilidade
+    	*/
+   	int getDisponibilidade() const { return disponibilidade; }
+
+	/** Método de alteração de disponibilidade.
+ 	* \param disponibilidade Novo valor de disponibilidade
+ 	*/
+	void setDisponibilidade(int disponibilidade) { this->disponibilidade = disponibilidade; }
 
 	
 private:
