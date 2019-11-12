bbb66e5a569f3ed806f6a1fb37d7f4a068674f25
MatTerra <mateus.b.s.terra@gmail.com>
2019-11-12 10:45:08 -0300

    New exception

---
+++ src/propertynotfoundexception.h
@@ -0,0 +1,20 @@
+#ifndef PROPERTYNOTFOUNDEXCEPTION_H
+#define PROPERTYNOTFOUNDEXCEPTION_H
+
+#include <exception>
+
+
+class PropertyNotFoundException : public std::exception{
+  /** Exceção personalizada para detecção de problemas de campos do banco
+    */
+
+  public:
+    /** Método para retorno de rastro da exceção e mensagem personalizada.
+     * \return Mensagem personalizada descritiva do erro
+     */
+    const char * what () const noexcept {
+          return "The requested property doesn't exists";
+    }
+};
+
+#endif // PROPERTYNOTFOUNDEXCEPTION_H
