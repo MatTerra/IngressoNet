0d08f1edcb1fb0fbdb4cea4adee9839bf9861d70
MatTerra <mateus.b.s.terra@gmail.com>
2019-11-14 21:10:20 -0300

    Fixed button names for convention; Added validators for inputs; Implemented slots and signal for pre login validation

--- src/loginwindow.cpp
+++ src/loginwindow.cpp
@@ -3,9 +3,16 @@
 
 LoginWindow::LoginWindow(QWidget *parent) :
   QWidget(parent),
-  ui(new Ui::LoginWindow)
-{
+  ui(new Ui::LoginWindow){
   ui->setupUi(this);
+
+  QRegExp reCpf("^[0-9]{3}\\.[0-9]{3}\\.[0-9]{3}-[0-9]{2}$");
+  QRegExpValidator *validatorCpf = new QRegExpValidator(reCpf, this);
+  ui->cpfEdit->setValidator(validatorCpf);
+
+  ui->errorLabel->setStyleSheet("color:red;");
+
+  connect(this, SIGNAL(loginDataValidationNeeded(QString, QString)), this, SLOT(validateData(QString, QString)));
 }
 
 LoginWindow::~LoginWindow()
@@ -13,9 +20,46 @@ LoginWindow::~LoginWindow()
   delete ui;
 }
 
-void LoginWindow::on_signupButton_clicked()
-{
-    QMainWindow* mw = dynamic_cast<QMainWindow *>(parent());
-    mw->setCentralWidget(new SignupWindow(mw));
-    this->destroy();
+void LoginWindow::on_signupBtn_clicked(){
+  QMainWindow* mw = dynamic_cast<QMainWindow *>(parent());
+  mw->setCentralWidget(new SignupWindow(mw));
+  this->destroy();
+}
+
+void LoginWindow::on_loginBtn_clicked(){
+  QString cpf = ui->cpfEdit->text();
+  QString senha = ui->senhaEdit->text();
+  emit loginDataValidationNeeded(cpf, senha);
+}
+
+void LoginWindow::validateData(QString cpf, QString senha){
+  if(!Usuario::isValidCPF(cpf.toStdString())){
+    ui->errorLabel->setText("CPF inválido!");
+    return;
+  }
+  if(senha.length()<6){
+    if(ui->errorLabel->text() == ""){
+      ui->errorLabel->setText("Senha inválida!");
+    }
+    return;
+  }
+  emit validData(cpf, senha);
+}
+
+void LoginWindow::on_cpfEdit_textEdited(const QString &arg1){
+  if(ui->errorLabel->text() == "CPF inválido!"){
+    ui->errorLabel->setText("");
+  }
+  if(arg1.length() == 3 || arg1.length() == 7){
+    ui->cpfEdit->insert(".");
+  }
+  if(arg1.length() == 11){
+    ui->cpfEdit->insert("-");
+  }
+}
+
+void LoginWindow::on_senhaEdit_textEdited(const QString &arg1){
+  if(ui->errorLabel->text() == "Senha inválida!"){
+    ui->errorLabel->setText("");
+  }
 }
--- src/loginwindow.h
+++ src/loginwindow.h
@@ -3,7 +3,11 @@
 
 #include <QWidget>
 #include <QMainWindow>
+#include <QRegExp>
+#include <QRegExpValidator>
+
 #include "signupwindow.h"
+#include "usuario.h"
 
 namespace Ui {
   class LoginWindow;
@@ -22,13 +26,44 @@ class LoginWindow : public QWidget
     ~LoginWindow();
 
 
+  signals:
+    /** Sinal emitido após solicitação de login para solicitar validadação dos dados de entrada.
+     * \param cpf CPF fornecido
+     * \param senha Senha fornecida
+     */
+    void loginDataValidationNeeded(QString, QString);
+
+    /** Sinal emitido após verificação positiva dos dados de login.
+     * \param cpf CPF validado
+     * \param senha Senha validada
+     */
+    void validData(QString, QString);
+
+
   private slots:
     /** Slot de reação ao clique no botão de cadastro.
      */
-    void on_signupButton_clicked();
+    void on_signupBtn_clicked();
+
+    /** Slot de reação ao clique no botão de login.
+     */
+    void on_loginBtn_clicked();
+
+    /** Slot de verificação do usuário e senha.
+     * \param cpf CPF fornecido
+     * \param senha Senha fornecida
+     */
+    void validateData(QString, QString);
+
+    /** Slot para inserção automática de '.' e '-' e ajuste do errorLabel
+     * @param arg1 Texto do input
+     */
+    void on_cpfEdit_textEdited(const QString &arg1);
 
 
-  private:
+    void on_senhaEdit_textEdited(const QString &arg1);
+
+private:
     Ui::LoginWindow *ui; //!< Atributo de instância "ui". Representa a interface gráfica da tela de login.
 };
 
