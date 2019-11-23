0cfec0b9a8de06d0c8b6739a7c1263674e007451
MatTerra <mateus.b.s.terra@gmail.com>
2019-11-23 11:05:42 -0300

    Fixed issues; added get test

--- src/ingressodao.cpp
+++ src/ingressodao.cpp
@@ -3,80 +3,69 @@
 IngressoDAO* IngressoDAO::instance = nullptr;
 MySQLHelper* IngressoDAO::mysqlHelper = nullptr;
 
-IngressoDAO::~CidadeDAO(){
+IngressoDAO::~IngressoDAO(){
 
 }
 
 Ingresso IngressoDAO::get(std::string codigo){
 	try{
-		std::string query = "SELECT codIngresso, codPartida, usuarioIngresso FROM ingresso_t WHERE codIngresso = '"+cod+"';";
-		MYSQL_RES* res = mysqlHelper->query(query);
+	  std::string query = "SELECT idIngresso, cpf, idPartida FROM ingresso_t WHERE idIngresso = '"+codigo+"';";
+	  MYSQL_RES* res = mysqlHelper->query(query);
 
-		MYSQL_ROW row;
-		if((row = mysql_fetch_row(res))){
-			int cod = atoi(row[0]);
-			int codPartida = atoi(row[1]);
-			std::string nomeUsuario = row[2];
-
-			Partida partida(codPartida, cidade, preco, disponibilidade, responsavel, daata, jogo);
-			Cartao cartao(numCartao, codSeguranca);
-			Usuario usuario(cpf, senha, cartao);
-			Ingresso ingresso(cod, partida, usuario); //n sei como pegar os dados para criar os objetos para partida e usuário
-
-			mysql_free_result(res);
-			return ingresso;
-		}
-		else{
-			Estado e(-1, "");
-			Cartao cartao(-1, -1);
-      		Cidade cidade(-1 ,"", e);
-      		Usuario responsavel(-1, -1, cartao);
-      		Jogo jogo(-1, "", tipo);
-			Partida partida(-1, cidade, -1, -1, responsavel, data, jogo);
-			Usuário usuario(-1, -1, cartao);
-			Ingresso ingresso(-1, partida, usuário);
-		}
-	}	catch(NotAbleToConnectException& e) {
-    		throw e;
-		} catch (FailedQueryException& e){
-    		throw e;
+          MYSQL_ROW row;
+          if((row = mysql_fetch_row(res))){
+            int cod = atoi(row[0]);
+            Usuario u = UsuarioDAO::getInstance()->get(row[1]);
+            u.setSenha("");
+            Partida p = PartidaDAO::getInstance()->get(row[2]);
+            Ingresso ingresso(cod, p, u);
+            mysql_free_result(res);
+            return ingresso;
+          }
+          else{
+            Usuario usuario("","");
+            Estado e(-1,"");
+            Cidade cidade(-1,"", e);
+            Jogo jogo(-1,"",Jogo::Tipo(0));
+            Partida partida(-1, cidade,-1,-1,usuario,QDateTime(),jogo);
+            Ingresso ingresso(-1, partida, usuario);
+            return ingresso;
+          }
+        } catch(NotAbleToConnectException& e) {
+          throw e;
+        } catch (FailedQueryException& e){
+          throw e;
     	}
 }
 
-std::vector<Ingresso> IngrssoDAO::getByProperty(std::string property, std::string value){
+std::vector<Ingresso> IngressoDAO::getByProperty(std::string property, std::string value){
 	std::vector<Ingresso> ingressos;
-	if(property.compare("codIngresso") != 0 && property.compare("codPartida") != 0 && property.compare("idUsuario") != 0){
-		throw new PropertyNotFoundException();
+	if(property.compare("idIngresso") != 0 && property.compare("cpf") != 0 && property.compare("idPartida") != 0){
+	  throw new PropertyNotFoundException();
 	}
 	try{
-		std::string query = "SELECT codIngresso, codPartida, idUsuario FROM ingresso_t WHERE "+property+" = '"+value+"';";
-		MYSQL_RES* res = mysqlHelper->query(query);
-		MYSQL_ROW row;
-		while((row = mysql_fetch_row(res))){
-			int cod = atoi(row[0]);
-			int codPartida = atoi(row[1]);
-			std::string nomeUsuario = row[2];
-
-			Partida partida(codPartida, cidade, preco, disponibilidade, responsavel, daata, jogo);
-			Cartao cartao(numCartao, codSeguranca);
-			Usuario usuario(cpf, senha, cartao);
-			Ingresso ingresso(cod, partida, usuario); //n sei como pegar os dados para criar os objetos para partida e usuário
-
-			ingressos.push_back(ingresso);
-		}
-		if(ingressos.size()==0){
-			Estado e(-1, "");
-			Cartao cartao(-1, -1);
-      		Cidade cidade(-1 ,"", e);
-      		Usuario responsavel(-1, -1, cartao);
-      		Jogo jogo(-1, "", tipo);
-			Partida partida(-1, cidade, -1, -1, responsavel, data, jogo);
-			Usuário usuario(-1, -1, cartao);
-
-			ingressos.push_back(Ingresso ingresso(-1, partida, usuário));
-		}
-		mysql_free_result(res);
-		return ingressos;
+	  std::string query = "SELECT idIngresso, cpf, idPartida FROM ingresso_t WHERE "+property+" = '"+value+"';";
+	  MYSQL_RES* res = mysqlHelper->query(query);
+	  MYSQL_ROW row;
+	  while((row = mysql_fetch_row(res))){
+	    int cod = atoi(row[0]);
+	    Usuario u = UsuarioDAO::getInstance()->get(row[1]);
+	    u.setSenha("");
+	    Partida p = PartidaDAO::getInstance()->get(row[2]);
+	    Ingresso ingresso(cod, p, u);
+	    ingressos.push_back(ingresso);
+	  }
+	  if(ingressos.size()==0){
+	    Usuario usuario("","");
+	    Estado e(-1,"");
+	    Cidade cidade(-1,"", e);
+	    Jogo jogo(-1,"",Jogo::Tipo(0));
+	    Partida partida(-1, cidade,-1,-1,usuario,QDateTime(),jogo);
+	    Ingresso ingresso(-1, partida, usuario);
+	    ingressos.push_back(ingresso);
+	  }
+	  mysql_free_result(res);
+	  return ingressos;
 	} catch (NotAbleToConnectException& e) {
     	throw e;
   	} catch (FailedQueryException& e){
@@ -87,24 +76,29 @@ std::vector<Ingresso> IngrssoDAO::getByProperty(std::string property, std::strin
 std::vector<Ingresso> IngressoDAO::getAll(){
 	std::vector<Ingresso> ingressos;
 	try {
-		std::string query = "SELECT codIngresso, codPartida, idUsuario FROM ingresso_t;";
-		MYSQL_RES* res = mysqlHelper->query(query);
-		MYSQL_ROW row = mysql_fetch_row(res);
+	  std::string query = "SELECT idIngresso, cpf, idPartida FROM ingresso_t;";
+	  MYSQL_RES* res = mysqlHelper->query(query);
+	  MYSQL_ROW row = mysql_fetch_row(res);
 
-		do{
-			int cod = atoi(row[0]);
-			int codPartida = atoi(row[1]);
-			std::string nomeUsuario = row[2];
-
-			Partida partida(codPartida, cidade, preco, disponibilidade, responsavel, daata, jogo);
-			Cartao cartao(numCartao, codSeguranca);
-			Usuario usuario(cpf, senha, cartao);
-			Ingresso ingresso(cod, partida, usuario); //n sei como pegar os dados para criar os objetos para partida e usuário
-
-			ingressos.push_back(ingresso);
-		}while((row = mysql_fetch_row(res)));
-		mysql_free_result(res);
-		return ingressos;
+	  while((row = mysql_fetch_row(res))){
+	    int cod = atoi(row[0]);
+	    Usuario u = UsuarioDAO::getInstance()->get(row[1]);
+	    u.setSenha("");
+	    Partida p = PartidaDAO::getInstance()->get(row[2]);
+	    Ingresso ingresso(cod, p, u);
+	    ingressos.push_back(ingresso);
+	  }
+	  if(ingressos.size()==0){
+	    Usuario usuario("","");
+	    Estado e(-1,"");
+	    Cidade cidade(-1,"", e);
+	    Jogo jogo(-1,"",Jogo::Tipo(0));
+	    Partida partida(-1, cidade,-1,-1,usuario,QDateTime(),jogo);
+	    Ingresso ingresso(-1, partida, usuario);
+	    ingressos.push_back(ingresso);
+	  }
+	  mysql_free_result(res);
+	  return ingressos;
 	} catch (NotAbleToConnectException& e) {
 		throw e;
 	} catch (FailedQueryException& e){
@@ -114,7 +108,7 @@ std::vector<Ingresso> IngressoDAO::getAll(){
 
 void IngressoDAO::save(Ingresso ingresso){
 	try {
-		std::string query = "INSERT INTO ingresso_t (codIngresso, codPartida, idUsuario) VALUES ('"+std::to_string(ingresso.getCodigo())+"', '"+std::to_string(ingresso.getPartida().getCodigo())+"', '"+ingresso.getUsuario().getCPF()+"';";
+		std::string query = "INSERT INTO ingresso_t (idIngresso, cpf, idPartida) VALUES ('"+std::to_string(ingresso.getCodigo())+"', '"+ingresso.getUsuario().getCPF()+"', '"+std::to_string(ingresso.getPartida().getCodigo())+"';";
 		mysql_free_result(mysqlHelper->query(query));
 	} catch (NotAbleToConnectException& e) {
 		throw e;
@@ -125,15 +119,11 @@ void IngressoDAO::save(Ingresso ingresso){
 
 void IngressoDAO::update(Ingresso ingresso, std::string field, std::string value){
 	try {
-		std::string query;
-		if(field.compare("codPartida")==0){
-			query = "UPDATE ingresso_t SET codPartida = '"+value+"' WHERE (codIngresso = '"+std::to_string(ingresso.getCodigo())+"');";
-		} else if(field.compare("idUsuario")==0){
-			query = "UPDATE cidade_t SET idUsuario = '"+value+"' WHERE (codIngresso = '"+std::to_string(cidade.getCodigo())+"');";
-		} else{
-			throw PropertyNotFoundException();
-		}
-		mysql_free_result(mysqlHelper->query(query));
+	  if(field.compare("idIngresso") != 0 && field.compare("cpf") != 0 && field.compare("idPartida") != 0){
+	    throw new PropertyNotFoundException();
+	  }
+	  std::string query = "UPDATE ingresso_t SET "+field+" = '"+value+"' WHERE idIngresso = '"+std::to_string(ingresso.getCodigo())+"';";
+	  mysql_free_result(mysqlHelper->query(query));
 	} catch (NotAbleToConnectException& e) {
 		throw e;
 	} catch (FailedQueryException& e){
@@ -143,24 +133,24 @@ void IngressoDAO::update(Ingresso ingresso, std::string field, std::string value
 
 void IngressoDAO::remove(Ingresso ingresso){
 	try {
-		std::string query = "DELETE FROM ingresso_t WHERE (codIngresso = '"+std::to_string(ingresso.getCodigo())+"');";
-		mysql_free_result(mysqlHelper->query(query));
+	  std::string query = "DELETE FROM ingresso_t WHERE (idIngresso = '"+std::to_string(ingresso.getCodigo())+"');";
+	  mysql_free_result(mysqlHelper->query(query));
 	} catch (NotAbleToConnectException& e) {
-		throw e;
+	  throw e;
 	} catch (FailedQueryException& e){
-		throw e;
+	  throw e;
 	}
 }
 
 IngressoDAO* IngressoDAO::getInstance(){
-	if(IngressoDAO::instance == nullptr){
-		IngressoDAO::instance = new IngressoDAO();
-		try {
-			IngressoDAO::mysqlHelper = MySQLHelper::getInstance();
-		} catch (NotAbleToConnectException& e) {
-		IngressoDAO::instance = nullptr;
-		throw e;
-		}
-	}
-	return IngressoDAO::instance;
+  if(IngressoDAO::instance == nullptr){
+    IngressoDAO::instance = new IngressoDAO();
+    try {
+      IngressoDAO::mysqlHelper = MySQLHelper::getInstance();
+    } catch (NotAbleToConnectException& e) {
+      IngressoDAO::instance = nullptr;
+      throw e;
+    }
+  }
+  return IngressoDAO::instance;
 }
--- test/IngressoDAO/IngressoDAO.pro
+++ test/IngressoDAO/IngressoDAO.pro
@@ -5,7 +5,14 @@ SOURCES +=  \
     testeingressodao.cpp \
     ../../src/ingressodao.cpp \
     ../../src/ingresso.cpp \
-    ../../src/mysqlhelper.cpp
+    ../../src/mysqlhelper.cpp \
+    ../../src/usuariodao.cpp \
+    ../../src/partidadao.cpp \
+    ../../src/cidade.cpp \
+    ../../src/estado.cpp \
+    ../../src/usuario.cpp \
+    ../../src/partida.cpp \
+    ../../src/cartao.cpp
 LIBS += -lmariadb
 INCLUDEPATH += ../../src
 
